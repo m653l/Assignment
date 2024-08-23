0536408cec56fec725c474ba63324c5c6539fa66
m653l <sebastian.kura@pm.me>
2024-08-23 14:41:21 +0200

    Add project files.

---
+++ Infrastructure/Database/DbConnectionFactory.cs
@@ -0,0 +1,15 @@
+﻿using Npgsql;
+using System.Data;
+using Application.Abstractions.Data;
+
+namespace Infrastructure.Database;
+
+internal sealed class DbConnectionFactory(NpgsqlDataSource dataSource) : IDbConnectionFactory
+{
+    public IDbConnection GetOpenConnection()
+    {
+        NpgsqlConnection connection = dataSource.OpenConnection();
+
+        return connection;
+    }
+}
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,62 @@
+﻿using Application.Abstractions.Data;
+using Dapper;
+using Infrastructure.Database;
+using Infrastructure.Time;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using SharedKernel;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration) =>
+        services
+            .AddServices()
+            .AddDatabase(configuration)
+            .AddHealthChecks(configuration);
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
+
+        string? connectionString = configuration.GetConnectionString("Database");
+        Ensure.NotNullOrEmpty(connectionString);
+
+        services.AddSingleton<IDbConnectionFactory>(_ =>
+            new DbConnectionFactory(new NpgsqlDataSourceBuilder(connectionString).Build()));
+
+        services.AddDbContext<ApplicationDbContext>(
+            options => options
+                .UseNpgsql(connectionString, npgsqlOptions =>
+                    npgsqlOptions.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Default))
+                .UseSnakeCaseNamingConvention());
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        // services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddHealthChecks()
+            .AddNpgSql(configuration.GetConnectionString("Database")!);
+
+        return services;
+    }
+}
